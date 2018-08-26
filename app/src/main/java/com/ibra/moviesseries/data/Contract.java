9bb3cd0d4120b70d766e6aaed7c171ee3931cc4a
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-08-26 19:39:36 +0200

    remove api key

--- app/src/main/java/com/ibra/moviesseries/data/Contract.java
+++ app/src/main/java/com/ibra/moviesseries/data/Contract.java
@@ -13,7 +13,7 @@ public class Contract {
     public static final String POPULAR = "popular";
     public static final String ON_THE_AIR = "on_the_air";
     public static final String AIRING_TODAY = "airing_today";
-    public static final String API_KEY = "b74d1d1ae9314a57ace0a639dfff09ef";
+    public static final String API_KEY = "";
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String LOAD_MOVIES = BASE_URL+"movie/{cat}?api_key="+API_KEY;
     public static final String LOAD_TVS = BASE_URL+"tv/{cat}?api_key="+API_KEY;
